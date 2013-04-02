394fc5699d5f484047c5b35331271a41c6af3309
Ryan Walker <ryan.walker@rackspace.com>
2013-04-02 04:37:04 -0500

    Cleaning up config options.

--- lib/rubeez/config.rb
+++ lib/rubeez/config.rb
@@ -10,8 +10,6 @@ module Rubeez
     rubeez_file File.expand_path('~/.rubeez')
     private_key File.expand_path('~/.ssh/fog_rsa')
     public_key File.expand_path('~/.ssh/fog_rsa.pub')
-    num_requests "100"
-    concurrent_requests "10"
     headers ""
   end
 end
